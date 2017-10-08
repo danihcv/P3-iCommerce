3afd6ba7ccfa7583189314a1bdac9979773bcc61
danihcv <daniel.vassalo@hotmail.com.br>
2017-10-08 20:43:52 -0300

    Tela "Novo produto" Inputs sendo tratados e erros sendo emitidos.

--- iCommerce_Front/src/app/components/new-product/new-product.component.ts
+++ iCommerce_Front/src/app/components/new-product/new-product.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ProductService} from '../../services/product.service';
+import {Router} from '@angular/router';
+import {Product} from '../../models/product.model';
 
 @Component({
   selector: 'new-product',
@@ -16,8 +18,10 @@ export class NewProductComponent implements OnInit {
   categoryList: string[];
   createNewCategory = false;
   newCategory: string = '';
+  gotError = {};
 
-  constructor(private productService: ProductService) {
+  constructor(private router: Router,
+              private productService: ProductService) {
     productService.getCategoryList().subscribe( (data: string[]) => {
       this.categoryList = data;
     });
@@ -25,9 +29,29 @@ export class NewProductComponent implements OnInit {
 
   ngOnInit() {}
 
+  resetErrors(key?: string) {
+    if (key !== undefined) {
+      this.gotError[key] = false;
+    } else {
+      this.gotError['name'] = false;
+      this.gotError['image'] = false;
+      this.gotError['description'] = false;
+      this.gotError['category'] = false;
+      this.gotError['price'] = false;
+    }
+  }
+
   submit() {
     this.productService.createProduct({'name': this.name, 'image': this.image, 'description': this.description,
-      'price': this.price, 'category': this.createNewCategory ? this.newCategory : this.category, 'stock': this.stock}).subscribe();
+      'price': this.price, 'category': this.createNewCategory ? this.newCategory : this.category, 'stock': this.stock})
+      .subscribe(() => this.router.navigate(['']), (err) => {
+      let res = JSON.parse(err._body);
+      console.log(res);
+      this.resetErrors();
+      for (let k in res) {
+        this.gotError[k] = true;
+      }
+      });
   }
 
   change(data) {
